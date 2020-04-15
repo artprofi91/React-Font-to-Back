828abe558f3930f5d94d350bf89e1c1108393ae5
Tregubchak, Artem <artem.tregubchak@accenture.com>
2020-04-14 21:43:00 -0500

    contact keeper - no BE

---
+++ Contact-Keeper/client/src/components/contacts/Contacts.js
@@ -0,0 +1,28 @@
+import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import ContactItem from './ContactItem';
+import ContactContext from '../../context/contact/contactContext';
+
+export default function Contacts() {
+	const contactContext = useContext(ContactContext);
+	const { contacts, filtered } = contactContext;
+	if (contacts.length === 0) return <h4>Please add a contact</h4>;
+
+	return (
+		<Fragment>
+			<TransitionGroup>
+				{filtered !== null
+					? filtered.map((contact) => (
+							<CSSTransition key={contact.id} classNames="item" timeout={500}>
+								<ContactItem contact={contact} />
+							</CSSTransition>
+					  ))
+					: contacts.map((contact) => (
+							<CSSTransition key={contact.id} classNames="item" timeout={500}>
+								<ContactItem contact={contact} />
+							</CSSTransition>
+					  ))}
+			</TransitionGroup>
+		</Fragment>
+	);
+}
