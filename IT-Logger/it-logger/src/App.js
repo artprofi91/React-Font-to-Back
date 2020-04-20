cef855a9ec645362510a551ad1f191b64e0ca872
Tregubchak, Artem <artem.tregubchak@accenture.com>
2020-04-19 20:51:19 -0500

    added UI for IT-Logger project

--- IT-Logger/it-logger/src/App.js
+++ IT-Logger/it-logger/src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect, Fragment } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 const App = () => {
-	return <div className="App">My App</div>;
+	useEffect(() => {
+		//Init Materialize-css
+		M.AutoInit();
+	});
+	return (
+		<Fragment>
+			<SearchBar />
+			<div className="container">
+				<AddBtn />
+				<AddLogModal />
+				<EditLogModal />
+				<AddTechModal />
+				<TechListModal />
+				<Logs />
+			</div>
+		</Fragment>
+	);
 };
 
 export default App;
